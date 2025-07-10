bda86f346f3d9106c7b4c23b4eee5e3a9e4fe9aa
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-10 14:49:48 +0300

    cleaning up

--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:syncy/controllers/home_controller.dart';
-import 'package:syncy/utils/storage_helper.dart';
 
 class HomeBinding extends Bindings {
   @override
