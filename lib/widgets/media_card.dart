88eec15c5987eb61319150a947e9d494f8dc5bcf
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-25 10:00:04 +0300

    added styling and support for audios

--- lib/widgets/media_card.dart
+++ lib/widgets/media_card.dart
@@ -5,33 +5,30 @@ import 'package:flutter/material.dart';
 import 'package:syncy/models/media.dart';
 
 class MediaCard extends StatelessWidget {
+  final Media mediaElement;
   final void Function()? onPressed;
+  final bool isAudio;
 
   const MediaCard({
     super.key,
     required this.mediaElement,
     this.onPressed,
+    this.isAudio = false,
   });
-
-  final Media mediaElement;
 
   @override
   Widget build(BuildContext context) {
+    final bool hasThumbnail = mediaElement.thumbnailPath.isNotEmpty;
+
     return Container(
       decoration: BoxDecoration(
-        image: mediaElement.thumbnailPath != ''
+        color: isAudio ? Colors.deepPurple.shade300 : null,
+        image: !isAudio && hasThumbnail
             ? DecorationImage(
-                image: FileImage(
-                  File(mediaElement.thumbnailPath),
-                ),
+                image: FileImage(File(mediaElement.thumbnailPath)),
                 fit: BoxFit.cover,
               )
-            : DecorationImage(
-                image: AssetImage(
-                  'assets/broken_thumb.png',
-                ),
-                fit: BoxFit.cover,
-              ),
+            : null,
         borderRadius: BorderRadius.circular(22),
       ),
       child: Align(
@@ -41,7 +38,7 @@ class MediaCard extends StatelessWidget {
             GestureDetector(
               onTap: onPressed,
               child: ClipRRect(
-                borderRadius: BorderRadius.only(
+                borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(22),
                   bottomRight: Radius.circular(22),
                 ),
@@ -54,12 +51,27 @@ class MediaCard extends StatelessWidget {
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(
-                            left: 8,
-                            right: 8,
-                            bottom: 4,
-                            top: 20,
+                            left: 8, right: 8, bottom: 4, top: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                isAudio ? Icons.music_note : Icons.videocam,
+                                size: 18,
+                                color: Colors.white70,
+                              ),
+                              const SizedBox(width: 6),
+                              Flexible(
+                                child: Text(
+                                  mediaElement.name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
                           ),
-                          child: Text(mediaElement.name),
                         ),
                       ],
                     ),
@@ -72,24 +84,25 @@ class MediaCard extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.only(right: 6),
                 child: Transform.translate(
-                  offset: Offset(0, -22),
+                  offset: const Offset(0, -22),
                   child: Container(
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
-                        colors: [
-                          Colors.purple,
-                          Colors.deepPurpleAccent,
-                        ],
+                        colors: isAudio
+                            ? [Colors.pinkAccent, Colors.deepPurpleAccent]
+                            : [Colors.purple, Colors.deepPurple],
                       ),
                       borderRadius: BorderRadius.circular(100),
                     ),
                     child: IconButton(
                       onPressed: onPressed,
-                      icon: Icon(Icons.play_arrow_rounded),
+                      icon: Icon(
+                        isAudio ? Icons.headphones_rounded : Icons.play_arrow_rounded,
+                        color: Colors.white,
+                      ),
                       padding: EdgeInsets.zero,
                       style: IconButton.styleFrom(
-                        tapTargetSize:
-                            MaterialTapTargetSize.shrinkWrap,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                       ),
                     ),
                   ),
